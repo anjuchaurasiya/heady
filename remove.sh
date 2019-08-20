d9e858a1fede4aca1553a6d5e240ad25e98f9ca9
anjuchaurasiya <44491002+anjuchaurasiya@users.noreply.github.com>
2019-08-20 18:15:52 +0530

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -6,3 +6,4 @@ docker ps -a | egrep 'load|Exited' | cut -d" " -f1 | xargs docker rm 2>/dev/null
 docker images | sed 1d | grep -v "jenkins" | awk '{print $3}' | xargs docker rmi -f 2>/dev/null
 return 0
 }
+remove
